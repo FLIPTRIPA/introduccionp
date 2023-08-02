6a6b5b99c6fd74114ecaa62471599ffc4a265800
FLIPTRIPA <141078695+FLIPTRIPA@users.noreply.github.com>
2023-08-02 22:58:50 +0000

    clase 1

--- 1ejercicio.py
+++ 1ejercicio.py
@@ -1,1 +1,21 @@
+#1 ejercicio
+"""print("hola, mundo")
+#2 ejercicio
+mar = "hola, mundo"
+print(mar)"""
+
+#3 ejercicio
+"""nombre=input("como te llamas")
+print("hola "+nombre+"¡")"""
+
+#4 ejercicio
+"""print(((3+2)/(2*5))**2)"""
+
+#5 ejercicio 
+"""horas=float(input("que tanto ha trabajado"))
+costo=float(input("el pago es semanal"))
+pago=horas*costo
+print("tu total es:",pago)"""
+
+#6 ejercicio
 
