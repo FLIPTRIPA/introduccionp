1c1fea1827976ab52eb841c0298ddd1a88ba4cfc
FLIPTRIPA <141078695+FLIPTRIPA@users.noreply.github.com>
2023-08-03 00:01:59 +0000

    ejercicio1

--- 1ejercicio.py
+++ 1ejercicio.py
@@ -17,5 +17,21 @@ costo=float(input("el pago es semanal"))
 pago=horas*costo
 print("tu total es:",pago)"""
 
-#6 ejercicio
-
+#6 ejercicion
+"""n=float(input("entero positivo"))
+suma= n*(n+1)/2
+print("el resultado de la suma del entero:",suma)"""
+#7 ejercicio
+"""kg=input("coloque su peso en kg:")
+estatura=input("coloque su estatura:")
+imc=round(float(kg)/float(estatura)**2,2)
+print("su masa corporal es:",imc)"""
+#9 ejercicio
+"""cantidad= float(input("cantidad que quiere invertir:"))
+interes=float(input("interes acumulado durante el año:"))
+años=float(input("numero de años"))
+print("capital obtenido",cantidad*(interes/100+1)**años)"""
+#10 ejercicio
+muñecas=float(input("cantidad de muñecas"))
+pesO=float(input(muñeca)*75)
+payasos=float(input("cantidad de payasos"))
